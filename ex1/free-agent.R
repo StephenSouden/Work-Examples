8360fb78ae91849356693d8db2a461e97546f8b8
StephenSouden <stephensouden@gmail.com>
2023-10-30 12:07:08 -0400

    init free agent example'

---
+++ ex1/free-agent.R
@@ -0,0 +1,15 @@
+install.packages("dplyr")
+install.packages("ggplot2")
+library(dplyr)
+library(ggplot2)
+install.packages("tidyverse")
+library(tidyverse)
+
+ggplot(data = fawar, aes(x = Year, y = DollarWAR, color = Pos)) +
+  geom_point() +
+  scale_x_continuous(breaks = seq(2010,2023,by = 1)) +
+  scale_y_continuous(breaks = seq(5.5,14,by = .5)) +
+  labs(x = 'Year', y = 'FA Spending on WAR (in millions of $)',
+        title = 'Separated Free Agent Value of Batters and Pitchers, 2010-2023',
+        color = 'Position') +
+  geom_smooth(method = 'lm', se = FALSE)
